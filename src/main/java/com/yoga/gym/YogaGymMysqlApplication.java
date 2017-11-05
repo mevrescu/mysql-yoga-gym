a45a2019676dd94541b4fafd0da256196c40fbc4
cimpapps <catalincimpoeru24@gmail.com>
2017-11-05 11:04:15 +0200

    start project on mysql

---
+++ src/main/java/com/yoga/gym/YogaGymMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.yoga.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaGymMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaGymMysqlApplication.class, args);
+	}
+}
